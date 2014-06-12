623e9985b63d8ca86b78afd87e8780869ba130ae
Jameson <jamesonjlee@gmail.com>
2014-06-12 16:06:54 -0700

    only need to restart if we change mnesiadir or something

--- test/cookbooks/rabbitmq_test/recipes/user_lwrps.rb
+++ test/cookbooks/rabbitmq_test/recipes/user_lwrps.rb
@@ -1,15 +1,6 @@
 include_recipe 'rabbitmq::default'
 include_recipe 'rabbitmq::user_management'
 
-# force restart the rabbitmq server, then sleep until it's ready(tm)
-service node['rabbitmq']['service_name'] do
-    action :restart
-end
-execute 'sleep 30' do
-  action :run
-end
-
-# can't verify it actually goes through without logging in, but at least exercise the code
 rabbitmq_user 'kitchen3' do
   password 'foobar'
   action :add
